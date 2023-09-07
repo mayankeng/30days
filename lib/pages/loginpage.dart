bc559b21639eeab65b0b43dade9937009102449b
mayanksalescode <mayank.aggarwal@salescode.ai>
2023-09-07 18:08:24 +0530

    day completed

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -9,15 +9,52 @@ class loginpage extends StatelessWidget {
       color: Colors.white,
       child: Column(
         children: [
-          Image.asset("assets/images/login_page.png", fit: BoxFit.cover,)
-        ], 
-        Text(
-          "welcome" , style: TextStyle(
-            fontSize: 22,
+          Image.asset("assets/images/login_page.png", fit: BoxFit.cover,),
+          SizedBox(height: 20,)// used to give height between image and text
+          Text("Welcom", style: TextStyle(
+            fontSize: 25,
             fontWeight: FontWeight.bold,
-          ),
-        )
+          ),),
+          SizedBox(
+            height: 20,
+            ),// used to give height between image and text
+            
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0 , horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                decoration: InputDecoration( 
+                  hintText: "Enter Username",
+                  label: "Username", 
+            
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration( 
+                  hintText: "Enter Password",
+                  label: "Password", 
+             
+                ),
+              ),
+              SizedBox(
+            height: 20,
+            ),
+
+            ElevatedButton(
+            child: Text("Login") ,
+            style: TextButton.styleFrom(),
+            onPressed: () {
+              print("Hi mayank");
+            }
+            )
+              ],
+              ),
+            )
+        ],
       )
+      ,
     );
   }
 } 
